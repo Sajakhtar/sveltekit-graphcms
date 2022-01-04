3a5fe7490f5414e217c1f1f7fce357f8d9fbdd5f
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-01-04 16:22:46 +0400

    create navbar component using daisyui navbars, place nav component in __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,11 @@
 <script>
 	import '../app.css';
+  import Nav from '$lib/nav.svelte'
 </script>
 
 <!-- global layout -->
+
+<Nav />
 
 <main class="container max-w-xl mx-auto px-4">
   <!-- content slots in -->
